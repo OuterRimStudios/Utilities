8908d0ff3d62c7deb75f4488f16f177459e00506
Sam Gebhard <gebhas6@gmail.com>
2019-04-10 10:39:42 -0500

    updated mathutilies, added targetutilities

--- MathUtilities.cs
+++ MathUtilities.cs
@@ -1,48 +1,50 @@
-﻿using UnityEngine;
+﻿//DOCUMENTATION:    https://github.com/OuterRimStudios/Utilities/wiki/Math-Utilities
+
+using UnityEngine;
 
 namespace OuterRimStudios.Utilities
 {
     public static class MathUtilities
     {
         #region MapValue
-        /// <summary>Converts the value passed in to a value within a 0 to 1 range.</summary>
-        /// <param name="min">Low end of the range that the value can be before converting to a 0-1 range.</param>
-        /// <param name="max">High end of the range that the value can be before converting to a 0-1 range.</param>
-        /// <param name="value">Value to be converted to a 0-1 range</param>
-        /// <returns>Returns input value converted to be a value between 0 and 1 as a float.</returns>
-        public static float MapValue01(float min, float max, float value)
+        /// <summary>Maps a value that exists between a range to be within a 0 to 1 range.</summary>
+        /// <param name="currentRangeMin">The minimum value of the range that the value currently exists.</param>
+        /// <param name="currentRangeMax">The maximum value of the range that the value currently exists.</param>
+        /// <param name="value">The value to be mapped within the new range.</param>
+        /// <returns>Returns input value converted to a value within a 0 to 1 range.</returns>
+        public static float MapValue01(float currentRangeMin, float currentRangeMax, float value)
         {
-            return min + (max - min) * ((value - 0) / (1 - 0));
+            return currentRangeMin + (currentRangeMax - currentRangeMin) * ((value - 0) / (1 - 0));
         }
 
-        /// <summary>Converts the value passed in to a value within an entered range.</summary>
-        /// <param name="range1Min">Low end of the range that the value will be converted to fit within.</param>
-        /// <param name="range1Max">High end of the range that the value will be converted to fit within.</param>
-        /// <param name="range2Min">Low end of the range that the value can be before converting to fit within the target range.</param>
-        /// <param name="range2Max">High end of the range that the value can be before converting to fit within the target range.</param>
-        /// <param name="value"></param>
-        /// <returns>Returns input value converted to be a value between entered range as a float.</returns>
-        public static float MapValue(float range1Min, float range1Max, float range2Min, float range2Max, float value)
+        /// <summary>Maps a value that exists between a range to be within a new range.</summary>
+        /// <param name="newRangeMin">The minimum value of the range within which the value will be mapped.</param>
+        /// <param name="newRangeMax">The maximum value of the range within which the value will be mapped.</param>
+        /// <param name="currentRangeMin">The minimum value of the range that the value currently exists.</param>
+        /// <param name="currentRangeMax">The maximum value of the range that the value currently exists.</param>
+        /// <param name="value">The value to be mapped within the new range.</param>
+        /// <returns>Returns input value converted to a value within new range.</returns>
+        public static float MapValue(float newRangeMin, float newRangeMax, float currentRangeMin, float currentRangeMax, float value)
         {
-            return range2Min + (range2Max - range2Min) * ((value - range1Min) / (range1Max - range1Min));
+            return currentRangeMin + (currentRangeMax - currentRangeMin) * ((value - newRangeMin) / (newRangeMax - newRangeMin));
         }
         #endregion
 
         #region Clamp
         /// <summary>Clamps all axes of the entered Vector3 between the negative and positive of the entered clamp value.</summary>
-        /// <param name="vector"></param>
-        /// <param name="clampValue"></param>
-        /// <returns>Returns the clamped Vector3.</returns>
+        /// <param name="vector">The Vector3 to be clamped.</param>
+        /// <param name="clampValue">The value at which the Vector3 will be clamped.</param>
+        /// <returns>Returns the Vector3 after being clamped within the clamp range.</returns>
         public static Vector3 Clamp(Vector3 vector, float clampValue)
         {
             return new Vector3(Mathf.Clamp(vector.x, -clampValue, clampValue), Mathf.Clamp(vector.y, -clampValue, clampValue), Mathf.Clamp(vector.z, -clampValue, clampValue));
         }
 
-        /// <summary>Clamps all axes of the entered Vector3 between the entered min clamp value and the entered max clamp value.</summary>
-        /// <param name="vector"></param>
-        /// <param name="minClampValue"></param>
-        /// <param name="maxClampValue"></param>
-        /// <returns>Returns the clamped Vector3.</returns>
+        /// <summary>Clamps all axes of the entered Vector3 between the minClampValue and the maxClampValue.</summary>
+        /// <param name="vector">The Vector3 to be clamped.</param>
+        /// <param name="minClampValue">The minimum value at which the Vector3 will be clamped.</param>
+        /// <param name="maxClampValue">The maximum value at which the Vector3 will be clamped.</param>
+        /// <returns>Returns the Vector3 after being clamped between the minClampValue and maxClampValue.</returns>
         public static Vector3 Clamp(Vector3 vector, float minClampValue, float maxClampValue)
         {
             return new Vector3(Mathf.Clamp(vector.x, minClampValue, maxClampValue), Mathf.Clamp(vector.y, minClampValue, maxClampValue), Mathf.Clamp(vector.z, minClampValue, maxClampValue));
@@ -51,9 +53,9 @@ namespace OuterRimStudios.Utilities
 
         #region CheckDistance
         /// <summary>Calculates the distance between two Vector3 points.</summary>
-        /// <param name="point1"></param>
-        /// <param name="point2"></param>
-        /// <returns>Distance between two points as a float</returns>
+        /// <param name="point1">The first point of the distance calculation.</param>
+        /// <param name="point2"> The second point of the distance calculation.</param>
+        /// <returns>Return the distance between the two points.</returns>
         public static float CheckDistance(Vector3 point1, Vector3 point2)
         {
             Vector3 heading;
@@ -75,9 +77,9 @@ namespace OuterRimStudios.Utilities
         }
 
         /// <summary>Calculates the distance between two Vector2 points.</summary>
-        /// <param name="point1"></param>
-        /// <param name="point2"></param>
-        /// <returns>Distance between two points as a float</returns>
+        /// <param name="point1">The first point of the distance calculation.</param>
+        /// <param name="point2">The second point of the distance calculation.</param>
+        /// <returns>Return the distance between the two points.</returns>
         public static float CheckDistance(Vector2 point1, Vector2 point2)
         {
             Vector2 heading;
---
+++ TargetUtilities.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OuterRimStudios.Utilities
+{
+    public class TargetUtilities
+    {
+        #region GetClosestObject
+        /// <summary>Calculates the closest object from a given array of GameObjects.</summary>
+        /// <param name="objects">Array of objects to compare distances.</param>
+        /// <param name="originPos">Position to check distance from.</param>
+        /// <returns>Transform of closest object.</returns>
+        public static Transform GetClosestObject(GameObject[] objects, Vector3 originPos)
+        {
+            Transform closestObject = null;
+            float minDist = Mathf.Infinity;
+            foreach (GameObject obj in objects)
+            {
+                float dist = MathUtilities.CheckDistance(obj.transform.position, originPos);
+                if (dist < minDist)
+                {
+                    closestObject = obj.transform;
+                    minDist = dist;
+                }
+            }
+            return closestObject;
+        }
+
+        /// <summary>Calculates the closest object from a given array of Transforms.</summary>
+        /// <param name="objects">Array of objects to compare distances.</param>
+        /// <param name="originPos">Position to check distance from.</param>
+        /// <returns>Transform of closest object.</returns>
+        public static Transform GetClosestObject(Transform[] objects, Vector3 originPos)
+        {
+            Transform closestObject = null;
+            float minDist = Mathf.Infinity;
+            foreach (Transform obj in objects)
+            {
+                float dist = MathUtilities.CheckDistance(obj.transform.position, originPos);
+                if (dist < minDist)
+                {
+                    closestObject = obj.transform;
+                    minDist = dist;
+                }
+            }
+            return closestObject;
+        }
+
+        /// <summary>Calculates the closest object from a given List of GameObjects.</summary>
+        /// <param name="objects">List of objects to compare distances.</param>
+        /// <param name="originPos">Position to check distance from.</param>
+        /// <returns>Transform of closest object.</returns>
+        public static Transform GetClosestObject(List<GameObject> objects, Vector3 originPos)
+        {
+            Transform closestObject = null;
+            float minDist = Mathf.Infinity;
+            foreach (GameObject obj in objects)
+            {
+                float dist = MathUtilities.CheckDistance(obj.transform.position, originPos);
+                if (dist < minDist)
+                {
+                    closestObject = obj.transform;
+                    minDist = dist;
+                }
+            }
+            return closestObject;
+        }
+
+        /// <summary>Calculates the closest object from a given List of Transforms.</summary>
+        /// <param name="objects">List of objects to compare distances.</param>
+        /// <param name="originPos">Position to check distance from.</param>
+        /// <returns>Transform of closest object.</returns>
+        public static Transform GetClosestObject(List<Transform> objects, Vector3 originPos)
+        {
+            Transform closestObject = null;
+            float minDist = Mathf.Infinity;
+            foreach (Transform obj in objects)
+            {
+                float dist = MathUtilities.CheckDistance(obj.transform.position, originPos);
+                if (dist < minDist)
+                {
+                    closestObject = obj.transform;
+                    minDist = dist;
+                }
+            }
+            return closestObject;
+        }
+        #endregion
+
+        #region GetClosestObjects
+        /// <summary>Calculates the closest objects from a given List of GameObjects.</summary>
+        /// <param name="objects">List of objects to compare distances.</param>
+        /// <param name="originPos">Position to check distance from.</param>
+        /// <param name="amount">Number of objects to return.</param>
+        /// <returns>List of Transforms of the closest objects.</returns>
+        public static List<Transform> GetClosestObjects(List<GameObject> objects, Vector3 originPos, int amount)
+        {
+            List<GameObject> tempObjects = new List<GameObject>();
+            List<Transform> closestsTargets = new List<Transform>();
+
+            foreach (GameObject go in objects)
+                tempObjects.Add(go);
+
+            for (int i = 0; i < amount; i++)
+            {
+                Transform closestObject = GetClosestObject(tempObjects, originPos);
+                closestsTargets.Add(closestObject);
+                tempObjects.Remove(closestObject.gameObject);
+            }
+            return closestsTargets;
+        }
+
+        /// <summary>Calculates the closest objects from a given List of Transforms.</summary>
+        /// <param name="objects">List of objects to compare distances.</param>
+        /// <param name="originPos">Position to check distance from.</param>
+        /// <param name="amount">Number of objects to return.</param>
+        /// <returns>List of Transforms of the closest objects.</returns>
+        public static List<Transform> GetClosestObjects(List<Transform> objects, Vector3 originPos, int amount)
+        {
+            List<Transform> tempObjects = new List<Transform>();
+            List<Transform> closestsTargets = new List<Transform>();
+
+            foreach (Transform t in objects)
+                tempObjects.Add(t);
+
+            for (int i = 0; i < amount; i++)
+            {
+                Transform closestObject = GetClosestObject(tempObjects, originPos);
+                closestsTargets.Add(closestObject);
+                tempObjects.Remove(closestObject);
+            }
+            return closestsTargets;
+        }
+
+        /// <summary>Calculates the closest objects from a given Array of GameObjects.</summary>
+        /// <param name="objects">Array of objects to compare distances.</param>
+        /// <param name="originPos">Position to check distance from.</param>
+        /// <param name="amount">Number of objects to return.</param>
+        /// <returns>Array of Transforms of the closest objects.</returns>
+        public static Transform[] GetClosestObjects(GameObject[] objects, Vector3 originPos, int amount)
+        {
+            List<GameObject> tempObjects = new List<GameObject>();
+            List<Transform> closestsTargets = new List<Transform>();
+
+            foreach (GameObject go in objects)
+                tempObjects.Add(go);
+
+            for (int i = 0; i < amount; i++)
+            {
+                Transform closestObject = GetClosestObject(tempObjects, originPos);
+                closestsTargets.Add(closestObject);
+                tempObjects.Remove(closestObject.gameObject);
+            }
+            return closestsTargets.ToArray();
+        }
+
+        /// <summary>Calculates the closest objects from a given Array of Transforms.</summary>
+        /// <param name="objects">Array of objects to compare distances.</param>
+        /// <param name="originPos">Position to check distance from.</param>
+        /// <param name="amount">Number of objects to return.</param>
+        /// <returns>Array of Transforms of the closest objects.</returns>
+        public static Transform[] GetClosestObjects(Transform[] objects, Vector3 originPos, int amount)
+        {
+            List<Transform> tempObjects = new List<Transform>();
+            List<Transform> closestsTargets = new List<Transform>();
+
+            foreach (Transform t in objects)
+                tempObjects.Add(t);
+
+            for (int i = 0; i < amount; i++)
+            {
+                Transform closestObject = GetClosestObject(tempObjects, originPos);
+                closestsTargets.Add(closestObject);
+                tempObjects.Remove(closestObject);
+            }
+            return closestsTargets.ToArray();
+        }
+        #endregion
+
+        #region GetPoint
+        /// <summary>Calculates a point along a ray, hitting specified layers.</summary>
+        /// <param name="origin">Starting position of the ray.</param>
+        /// <param name="direction">Direction the ray will be pointing.</param>
+        /// <param name="distance">Distance the ray will span.</param>
+        /// <param name="mask">Layers with which the ray can collide.</param>
+        /// <returns>Returns the closest point along the ray that meets a detectable layer.</returns>
+        public static Vector3 GetPoint(Vector3 origin, Vector3 direction, float distance, LayerMask mask)
+        {
+            RaycastHit hit;
+            Ray ray = new Ray(origin, direction);
+            if (Physics.Raycast(ray, out hit, distance, mask))
+            {
+                Vector3 point = new Vector3(origin.x, origin.y, hit.point.z + (hit.normal.z * 1.25f));
+                return point;
+            }
+            else return ray.GetPoint(distance);
+        }
+
+        /// <summary>Calculates a point along a ray at the specific distance.</summary>
+        /// <param name="origin">Starting position of the ray.</param>
+        /// <param name="direction">Direction the ray will be pointing.</param>
+        /// <param name="distance">Distance of the point on the ray from the origin.</param>
+        /// <returns>Returns a point along the ray at the given distance.</returns>
+        public static Vector3 GetPoint(Vector3 origin, Vector3 direction, float distance)
+        {
+            Ray ray = new Ray(origin, direction);
+            return ray.GetPoint(distance);
+        }
+        #endregion
+    }
+}
