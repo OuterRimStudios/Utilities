1dbe548a2cb75d6a5cc94aee27810fcf28891ffd
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-11-15 10:49:03 -0600

    added android support

--- AnalyticsUtilities.cs
+++ AnalyticsUtilities.cs
@@ -14,17 +14,24 @@ namespace OuterRimStudios.Utilities
         /// <param name="data">The data that you want written to the csv file."</param>
         public static void Event<T>(string eventName, List<T> data)
         {
+            string savePath = "";
+#if UNITY_EDITOR || UNITY_STANDALONE
+            savePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+#elif UNITY_ANDROID
+            savePath = Application.persistentDataPath;
+#endif
             //This bool controls whether or not the Write operation adds the headers for the data. If the file already exists, then it should already have the headers and this bool should remain false.
             bool createHeaders = false;
             //Adding the date to the filename so the analytics are separated based on date.
             string date = DateTime.Today.ToString("d").Replace('/', '-');
-            if (!Directory.Exists(Application.dataPath + "\\Analytics\\"))
-                Directory.CreateDirectory(Application.dataPath + "\\Analytics\\");
-            if (!File.Exists(Application.dataPath + "\\Analytics\\" + eventName + "Analytics_" + date + ".csv"))
+            string analyticsFolderPath = Path.Combine(savePath, "Analytics");
+            if (!Directory.Exists(analyticsFolderPath))
+                Directory.CreateDirectory(analyticsFolderPath);
+            if (!File.Exists(analyticsFolderPath + eventName + "Analytics_" + date + ".csv"))
                 createHeaders = true;
 
             //This section is writing the data to the csv file
-            using (var writer = new StreamWriter(Application.dataPath + "\\Analytics\\" + eventName + "Analytics_" + date + ".csv", append: true))
+            using (var writer = new StreamWriter(Path.Combine(analyticsFolderPath, eventName) + "Analytics_" + date + ".csv", append: true))
             using (var csv = new CsvWriter(writer))
             {
                 csv.Configuration.HasHeaderRecord = createHeaders;
